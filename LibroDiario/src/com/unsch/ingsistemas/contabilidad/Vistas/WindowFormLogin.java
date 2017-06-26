69357d8c867a772ffee7de850a5f327c928641f0
Jose Antonio Maldonado Candela <josephcand@gmail.com>
2017-06-26 07:18:18 -0500

    index

--- LibroDiario/src/com/unsch/ingsistemas/contabilidad/Vistas/WindowFormLogin.java
+++ LibroDiario/src/com/unsch/ingsistemas/contabilidad/Vistas/WindowFormLogin.java
@@ -1,5 +1,8 @@
 package com.unsch.ingsistemas.contabilidad.Vistas;
 
+import java.awt.Color;
+import javax.swing.JOptionPane;
+
 public class WindowFormLogin extends javax.swing.JFrame {
 
     public WindowFormLogin() {
@@ -14,16 +17,37 @@ public class WindowFormLogin extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
+        textInputPass1 = new jcmouse.materialdesign.TextInputPass();
+        textInput1 = new jcmouse.materialdesign.TextInput();
+        jButton1 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Usuario :");
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel2.setText("Contraseña : ");
+        jLabel1.setFont(new java.awt.Font("Yu Gothic UI Light", 1, 24)); // NOI18N
+        jLabel1.setText("CONTABILIDAD GENERAL");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel2.setText("USUARIO :");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 150, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel3.setText("CONTRASEÑA :");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 210, -1, -1));
+
+        textInputPass1.setColorPrimary(new java.awt.Color(0, 102, 255));
+        textInputPass1.setHint("Password");
+        textInputPass1.setMaxLength(16);
+        jPanel1.add(textInputPass1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 200, 190, -1));
+
+        textInput1.setColorPrimary(new java.awt.Color(0, 102, 255));
+        textInput1.setHint("Usuario");
+        textInput1.setMaxLength(16);
+        jPanel1.add(textInput1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 140, 190, -1));
 
         jButton1.setText("INGRESAR");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -31,50 +55,10 @@ public class WindowFormLogin extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 260, -1, -1));
 
-        jLabel3.setText("Sistema Contable - UNSCH - 2017");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
-                        .addGap(64, 64, 64)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jTextField1)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1)))
-                .addGap(85, 85, 85))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(115, 115, 115)
-                .addComponent(jLabel3)
-                .addContainerGap(139, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(46, 46, 46)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1)
-                .addContainerGap(66, Short.MAX_VALUE))
-        );
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/unsch/ingsistemas/contabilidad/Images/conta.jpg"))); // NOI18N
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -84,9 +68,7 @@ public class WindowFormLogin extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -94,10 +76,26 @@ public class WindowFormLogin extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 
-        WindowFormMain v = new WindowFormMain();
-        v.setLocationRelativeTo(null);
-        v.setVisible(true);
-        dispose();
+        if (this.textInput1.getText().trim().length() > 0) {
+            textInput1.setErrorEnabled(false);
+            if (this.textInput1.getText().trim().length() > 16) {
+                textInput1.setError("Longitud maxima de " + textInput1.getMaxLength() + " caracteres");
+                textInput1.setErrorEnabled(true);
+            } else if (this.textInputPass1.getText().trim().length() > 0) {
+                textInputPass1.setErrorEnabled(false);
+                textInput1.setErrorEnabled(false);
+                JOptionPane.showMessageDialog(this, "Usuario: " + textInput1.getText() + "\n"
+                        + "Contraseña: " + textInputPass1.getText() + "\ngracias!!!");
+            } else {
+                textInputPass1.setError("Debe escribir su contraseña");
+                textInputPass1.setErrorEnabled(true);
+            }
+        } else {
+            textInput1.setError("Debe escribir su nombre de usuario");
+            textInput1.setErrorEnabled(true);
+        }
+
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -140,8 +138,9 @@ public class WindowFormLogin extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private jcmouse.materialdesign.TextInput textInput1;
+    private jcmouse.materialdesign.TextInputPass textInputPass1;
     // End of variables declaration//GEN-END:variables
 }
